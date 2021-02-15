a0c9cb51cee68715bda9634a0665f4b7b89508d9
DmytroChernov05 <dmytro.chernov05@gmail.com>
2021-02-15 14:41:38 +0200

    add

---
+++ lb1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace lb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+{
+	MemoryNumber = 0;
+	ShowCalculator();
+}
+public static double MemoryNumber { get; set; }
+public static double Plus(double first, double second)
+{
+	return first + second;
+}
+public static double Minus(double first, double second)
+{
+	return first - second;
+}
+public static double Multiply(double first, double second)
+{
+	return first * second;
+}
+public static double Divide(double first, double second)
+{
+	if (second == 0)
+		throw new DivideByZeroException();
+	return first / second;
+}
+public static double Calculate(double first, double second, string operation)
+{
+	switch (operation)
+	{
+		case "+":
+			return Plus(first, second);
+		case "-":
+			return Minus(first, second);
+		case "*":
+			return Multiply(first, second);
+		case "/":
+			return Divide(first, second);
+		default:
+			throw new Exception("No such operation!");
+	}
+}
+public static void ShowCalculator()
+{
+	Console.WriteLine(MemoryNumber);
+	string operation = Console.ReadLine();
+	if (operation.Equals("exit"))
+	{
+		Environment.Exit(0);
+	}
+	try
+	{
+		double number = Convert.ToDouble(Console.ReadLine());
+		MemoryNumber = Calculate(MemoryNumber, number, operation);
+		Console.Clear();
+	}
+	catch (Exception exception)
+	{
+		Console.Clear();
+		Console.WriteLine(exception.Message);
+	}
+	finally
+	{
+		ShowCalculator();
+	}
+}
+    }
+}
